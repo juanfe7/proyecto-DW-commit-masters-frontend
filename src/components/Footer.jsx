24c07018bc1143b6f7851158bfbbbceecec4e02f
Martin Pereira <135734700+mpereira11@users.noreply.github.com>
2025-05-03 11:22:06 -0500

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4 text-center">
+      <p>© All rights reserved Commit Masters 2025</p>
+    </footer>
+  )
+}
+
+export default Footer 

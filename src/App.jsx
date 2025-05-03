bc80202cb8935460899b35e36d32dcb55f5e0c9d
Martin Pereira <135734700+mpereira11@users.noreply.github.com>
2025-05-03 17:55:19 -0500

    Update App.jsx
    Nuevas routes y direccioamiento. Correcta implementacion de routes.map

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { routes } from './config/routes'
 import { BackgroundProvider, useBackground } from './context/BackgroundContext'
-import { twMerge } from 'tailwind-merge'
-import { Navigate } from 'react-router-dom'
 
 const AppContent = () => {
   const { backgroundColor } = useBackground()
@@ -13,16 +11,12 @@ const AppContent = () => {
   const hideNavbarAndFooter = location.pathname === '/login'
 
   return (
-    <div className={twMerge('min-h-screen', backgroundColor)}>
+    <div className={`min-h-screen ${backgroundColor}`}>
       {!hideNavbarAndFooter && <Navbar />}
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         {routes.map((route) => (
-          <Route
-            key={route.id}
-            path={route.path}
-            element={<route.component />}
-          />
+          <Route key={route.id} path={route.path} element={<route.component />} />
         ))}
       </Routes>
       {!hideNavbarAndFooter && <Footer />}

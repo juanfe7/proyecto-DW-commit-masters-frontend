23d0de5ab762cb657fd3c05310029058404a3065
Martin Pereira <135734700+mpereira11@users.noreply.github.com>
2025-05-03 21:52:37 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,16 @@ const AppContent = () => {
   const hideNavbarAndFooter = location.pathname === '/login'
 
   return (
-    <div className={`min-h-screen ${backgroundColor}`}>
+    <div className={`flex flex-col min-h-screen ${backgroundColor}`}>
       {!hideNavbarAndFooter && <Navbar />}
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        {routes.map((route) => (
-          <Route key={route.id} path={route.path} element={<route.component />} />
-        ))}
-      </Routes>
+      <div className="flex-grow overflow-auto">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          {routes.map((route) => (
+            <Route key={route.id} path={route.path} element={<route.component />} />
+          ))}
+        </Routes>
+      </div>
       {!hideNavbarAndFooter && <Footer />}
     </div>
   )

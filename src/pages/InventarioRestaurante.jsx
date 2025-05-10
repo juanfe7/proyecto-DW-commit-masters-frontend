9e469434456440c01284ccbebca84e77e624dc9b
Martin Pereira <135734700+mpereira11@users.noreply.github.com>
2025-05-10 16:20:56 -0500

    cambios diseños

--- src/pages/InventarioRestaurante.jsx
+++ src/pages/InventarioRestaurante.jsx
@@ -45,7 +45,7 @@ const InventarioRestaurante = () => {
       {/* Encabezado */}
       <div className="flex justify-between items-center mb-6">
         {/* Título alineado a la izquierda */}
-        <h1 className="text-3xl font-bold">Inventario de: {ubicacion}</h1>
+        <h1 className="text-3xl font-bold">Inventario de {ubicacion}</h1>
 
         {/* Botones alineados a la derecha */}
         <div className="flex gap-4">
@@ -57,7 +57,7 @@ const InventarioRestaurante = () => {
           </button>
           <button
             onClick={handleVolverAtras}
-            className="bg-gray-600 text-white px-4 py-2 rounded-lg hover:bg-gray-700"
+            className="bg-[#041D64] text-white px-4 py-2 rounded-lg hover:bg-[#193F9E]"
           >
             Volver Atrás
           </button>

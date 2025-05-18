52a45397bf068a55c7b69121f2045f15d0d16cf3
Martin Pereira <135734700+mpereira11@users.noreply.github.com>
2025-05-18 00:15:13 -0500

    Quitar input del navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,17 +31,6 @@ const Navbar = () => {
         />
       </div>
 
-      {/* Input de búsqueda solo para cliente */}
-      {isClienteSection && (
-        <div className="flex-grow mx-4">
-          <input
-            type="text"
-            placeholder="Buscar comida, restaurantes, cafeterías..."
-            className="w-full px-4 py-2 rounded-lg text-black bg-[#ECEDF1]"
-          />
-        </div>
-      )}
-
       {/* Botones lado derecho */}
       <div className="flex items-center space-x-4">
         {/* Cliente: historial y carrito */}

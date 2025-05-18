d044176dea3806f636537b62c22fd26649e4bb98
Martin Pereira <135734700+mpereira11@users.noreply.github.com>
2025-05-18 00:31:43 -0500

    export Cliente Reseñas

---
+++ src/pages/ClienteReseñas.jsx
@@ -0,0 +1,10 @@
+const ClienteReseñas = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold text-[#041D64]">Dejar una Reseña</h1>
+      {/* Aquí irá el formulario */}
+    </div>
+  );
+};
+
+export default ClienteReseñas;

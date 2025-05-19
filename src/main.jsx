1ade4d028aa871505a9bbe0fa711a3bf6e9aa157
Martin Pereira <135734700+mpereira11@users.noreply.github.com>
2025-05-18 20:00:09 -0500

    Comentarios en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Renderiza la aplicacion React en el elemento con id 'root'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App /> {/* Componente principal de la aplicacion */}
   </StrictMode>,
 )
